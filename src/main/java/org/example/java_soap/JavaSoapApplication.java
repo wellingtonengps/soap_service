10fcfc1f909a84a7da5bc716cb7f5d361cd85e4c
Wellington Pereira <43380060+wellingtonengps@users.noreply.github.com>
2024-03-25 18:28:07 -0300

    first commit

---
+++ src/main/java/org/example/java_soap/JavaSoapApplication.java
@@ -0,0 +1,13 @@
+package org.example.java_soap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSoapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaSoapApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/java_soap/JavaSoapApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.java_soap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSoapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
