e5859e4bcf14f669568dec7090bcddee5d06ce98
Wellington Pereira <43380060+wellingtonengps@users.noreply.github.com>
2024-03-28 10:45:20 -0300

    feat: adicionado leitura de csv

--- src/main/java/org/example/java_soap/CountryEndpoint.java
+++ src/main/java/org/example/java_soap/CountryEndpoint.java
@@ -13,7 +13,7 @@ import io.spring.guides.gs_producing_web_service.GetCountryResponse;
 public class CountryEndpoint {
     private static final String NAMESPACE_URI = "http://spring.io/guides/gs-producing-web-service";
 
-    private CountryRepository countryRepository;
+    private final CountryRepository countryRepository;
 
     @Autowired
     public CountryEndpoint(CountryRepository countryRepository) {
--- src/main/java/org/example/java_soap/CountryRepository.java
+++ src/main/java/org/example/java_soap/CountryRepository.java
@@ -1,11 +1,18 @@
 package org.example.java_soap;
 
 import jakarta.annotation.PostConstruct;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
 import java.util.HashMap;
 import java.util.Map;
 
 import io.spring.guides.gs_producing_web_service.Country;
-import io.spring.guides.gs_producing_web_service.Currency;
+//import io.spring.guides.gs_producing_web_service.Currency;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
 import org.springframework.stereotype.Component;
 import org.springframework.util.Assert;
 
@@ -15,6 +22,30 @@ public class CountryRepository {
 
     @PostConstruct
     public void initData() {
+        try (Reader reader = new FileReader("src/main/java/org/example/java_soap/repository.csv")) {
+
+            CSVParser csvParser = new CSVParser(reader, CSVFormat.DEFAULT.withHeader("name", "flag", "maps", "region", "population", "currencies").withIgnoreHeaderCase().withTrim());
+
+            for (CSVRecord csvRecord : csvParser) {
+
+                Country country = new Country();
+
+                country.setName(csvRecord.get("name"));
+                country.setFlag(csvRecord.get("flag"));
+                country.setMaps(csvRecord.get("maps"));
+                country.setRegion(csvRecord.get("region"));
+                country.setPopulation(csvRecord.get("population"));
+                country.setCurrencies(csvRecord.get("currencies"));
+
+
+                countries.put(country.getName(), country);
+            }
+        } catch (IOException error) {
+            throw new RuntimeException(error);
+        }
+    }
+
+        /*
         Country spain = new Country();
         spain.setName("Spain");
         spain.setCapital("Madrid");
@@ -38,7 +69,7 @@ public class CountryRepository {
         uk.setPopulation(63705000);
 
         countries.put(uk.getName(), uk);
-    }
+    }*/
 
     public Country findCountry(String name) {
         Assert.notNull(name, "The country's name must not be null");
